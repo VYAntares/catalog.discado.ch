48f368c0f73a24c32f231c0b4123e8f23f4e54b1
VYAntares <endroahm@hotmail.com>
2026-02-25 00:44:40 +0100

    fix CI script: create Capacitor files manually instead of cap sync

--- ios/App/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/App/ci_scripts/ci_pre_xcodebuild.sh
@@ -10,7 +10,21 @@ cd "$CI_WORKSPACE"
 echo "==> Installing npm dependencies..."
 npm install
 
-echo "==> Running Capacitor sync..."
-npx cap sync ios
+echo "==> Setting up Capacitor iOS files..."
+
+# Create public directory (required by Xcode build)
+mkdir -p ios/App/App/public
+
+# Copy capacitor config to iOS platform
+cp capacitor.config.json ios/App/App/capacitor.config.json
+
+# Generate minimal config.xml
+node -e "
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('capacitor.config.json', 'utf8'));
+const xml = '<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<widget version=\"1.0.0\" xmlns=\"http://www.w3.org/ns/widgets\" xmlns:cdv=\"http://cordova.apache.org/ns/1.0\">\n    <name>' + config.appName + '</name>\n    <content src=\"index.html\" />\n    <access origin=\"*\" />\n</widget>';
+fs.writeFileSync('ios/App/App/config.xml', xml);
+console.log('config.xml created');
+"
 
 echo "==> Done."
